13f890fa5c1aa19c35def84450e66f9d8cd1635c
Alan Martins <alanmartins789@gmail.com>
2025-07-17 00:25:28 -0300

    feat: adicionando projetos e alguns ajustes no portifolio e implementando a acao de mudar o idioma

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ document.addEventListener('DOMContentLoaded', () => {
     .openPopup();
 });
 
+function toggleLanguage() {
+  const currentLang = localStorage.getItem("language") || "pt";
+  const newLang = currentLang === "pt" ? "en" : "pt";
+  setLanguage(newLang);
+  localStorage.setItem("language", newLang);
+}
+
+function setLanguage(lang) {
+  const isPT = lang === "pt";
+
+  document.getElementById("pt-title").style.display = isPT ? "inline" : "none";
+  document.getElementById("en-title").style.display = isPT ? "none" : "inline";
+
+  document.getElementById("pt-text").style.display = isPT ? "block" : "none";
+  document.getElementById("en-text").style.display = isPT ? "none" : "block";
+}
+
+// Carrega a linguagem salva ao iniciar
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("language") || "pt";
+  setLanguage(savedLang);
+});
+
+function toggleTheme() {
+  document.body.classList.toggle('light-theme');
+}
